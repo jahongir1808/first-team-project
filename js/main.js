8f34d6f2916932457a2ca1c43449ff50757d7b23
jahongir1808 <jahongir.developer18@gmail.com>
2022-12-25 19:47:13 +0500

    push

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function openNavbar() {
+  document.getElementById("navbar-responsive").style.left = "0";
+  // document.getElementById("navbar-responsive").style.top = "0";
+}
+function closeNavbar() {
+  document.getElementById("navbar-responsive").style.left = "-100%";
+  // document.getElementById("navbar-responsive").style.top = "-100%";
+}
