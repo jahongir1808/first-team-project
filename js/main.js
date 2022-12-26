a1a2035f6e5d4682ac5c25b7d6e0bfd29370965c
jahongir1808 <jahongir.developer18@gmail.com>
2022-12-26 14:34:34 +0500

    navbar change

--- js/main.js
+++ js/main.js
@@ -6,3 +6,15 @@ function closeNavbar() {
   document.getElementById("navbar-responsive").style.left = "-100%";
   // document.getElementById("navbar-responsive").style.top = "-100%";
 }
+let navbar = document.getElementById("navbar");
+
+function shrink() {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    navbar.classList.add("navbar-shrink");
+  } else {
+    navbar.classList.remove("navbar-shrink");
+  }
+}
